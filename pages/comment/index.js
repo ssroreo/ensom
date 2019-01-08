ad02b65ad668878a1286550b0ae5a57f78d91b3f
ssroreo <527191915@qq.com>
2019-01-08 14:37:33 +0800

    add

---
+++ pages/comment/index.js
@@ -0,0 +1,115 @@
+// pages/comment/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    textareaVal: '',       // textarea的文本值
+    min:5,
+    max:2000,
+    currentWordNumber:0,
+    texts: '至少5个字',
+    tiptext:''
+  },
+
+  textarea: function (e) {
+    var value = e.detail.value;
+    var len = parseInt(value.length);
+    if (len >= this.data.min && len != this.data.max){
+      this.setData({
+        texts: ""
+      })
+    } else if (len < this.data.min){
+      this.setData({
+        texts: "至少5个字"
+      })
+    } else{
+      this.setData({
+        texts: "最多输入2000个字"
+      })
+    }
+    this.setData({
+      textareaVal: value,
+      currentWordNumber: len
+    })
+  },
+
+  submitText:function(){
+    var that = this;
+    var value = that.data.textareaVal;
+    var len = parseInt(value.length);
+    if(len<5) {
+      that.setData({
+        tiptext:'发布失败，最少输入5个字！'
+      });
+      setTimeout(function(){
+        that.setData({
+          tiptext: ''
+        });
+      },1800);
+      return;
+    }
+    else {
+      wx.navigateBack({
+        
+      })
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,129 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    comment:[
+      {
+        id:1,
+        comment:'helloworld'
+      },
+      {
+        id:2,
+        comment:'你好'
+      },
+      {
+        id: 3,
+        comment: '你好\n你好啊\n12345678901234567890123456789012345678901234567890contentEditable\
+        你好\n你好啊\n12345678901234567890123456789012345678901234567890contentEditable\
+        你好\n你好啊\n12345678901234567890123456789012345678901234567890contentEditable'
+      },
+      {
+        id: 4,
+        comment: '12345678901234567890123456789012345678901234567890'
+      }, {
+        id: 5,
+        comment: 'helloworld'
+      },
+      {
+        id: 6,
+        comment: '你好'
+      },
+      {
+        id: 7,
+        comment: '你好\n你好啊\n12345678901234567890123456789012345678901234567890contentEditable\
+        你好\n你好啊\n12345678901234567890123456789012345678901234567890contentEditable\
+        你好\n你好啊\n12345678901234567890123456789012345678901234567890contentEditable'
+      },
+      {
+        id: 8,
+        comment: '12345678901234567890123456789012345678901234567890'
+      }, {
+        id: 9,
+        comment: 'helloworld'
+      },
+      {
+        id: 10,
+        comment: '你好'
+      },
+      {
+        id: 11,
+        comment: '你好\n你好啊\n12345678901234567890123456789012345678901234567890contentEditable\
+        你好\n你好啊\n12345678901234567890123456789012345678901234567890contentEditable\
+        你好\n你好啊\n12345678901234567890123456789012345678901234567890contentEditable'
+      },
+      {
+        id: 12,
+        comment: '12345678901234567890123456789012345678901234567890'
+      },
+    ]
+  },
+
+  aboutTap(){
+    console.log("22");
+  },
+
+  commentTap(){
+    wx.navigateTo({
+      url: '../comment/index',
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
